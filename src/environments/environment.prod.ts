0b644e3c1854496d7a2a8d1aecbe7ade41351a84
Trần Tâm <trantamttkmini@gmail.com>
2026-08-11 10:04:02 +0700

    tối ưu tốc độ load

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBase: 'https://socialapp-5rfu.onrender.com/api',
-  hubChat: 'https://socialapp-5rfu.onrender.com/hubs/chat',
-  hubNotify: 'https://socialapp-5rfu.onrender.com/hubs/notification',
+  apiBase: 'https://socialapp.trantamttkmini.workers.dev/api',
+  hubChat: 'https://socialapp.trantamttkmini.workers.dev/hubs/chat',
+  hubNotify: 'https://socialapp.trantamttkmini.workers.dev/hubs/notification',
 };
