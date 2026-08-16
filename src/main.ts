2770c643107cf4b680f3890e462672313d8eb629
Trần Tâm <trantamttkmini@gmail.com>
2026-08-16 14:40:16 +0700

    pwa

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,11 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').catch((err) => {
+      console.warn('SW registration failed:', err);
+    });
+  });
+}
