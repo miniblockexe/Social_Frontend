538ca35b547787f4472a1018c9cd6fc5ec71b22d
Trần Tâm <trantamttkmini@gmail.com>
2026-08-26 11:29:30 +0700

    post

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').catch((err) => {
-      console.warn('SW registration failed:', err);
-    });
-  });
-}
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err),
+);
